8f87f01548b4e11f42d62b1e809fbef3657b3ad7
darkistan <darkistan5@gmail.com>
2025-03-26 11:47:46 +0200

    GO

---
+++ Lesson 11 PW1 Task 5.py
@@ -0,0 +1,9 @@
+# Загальна кількість секунд
+total_seconds = 12345
+
+# Обчислюємо кількість хвилин і секунд
+minutes = total_seconds // 60  
+seconds = total_seconds % 60   
+
+# Виведення результату
+print(f"{total_seconds} секунд = {minutes} хвилин і {seconds} секунд")
--- main.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-current_datetime = datetime.now()
-print(current_datetime.date())
-print(current_datetime.time())
