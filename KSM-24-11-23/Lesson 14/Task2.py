2f313e633585efbca0b90a81da9aafb7700f7298
darkistan <darkistan5@gmail.com>
2025-04-09 10:14:02 +0300

    Lesson 14

---
+++ KSM-24-11-23/Lesson 14/Task2.py
@@ -0,0 +1,18 @@
+# Початкове значення
+value = 10
+
+temp1 = value
+value += 1  # тепер value = 11
+
+
+value -= 1  # тепер value = 10
+temp2 = value
+
+# Підсумовуємо обидва значення
+value = temp1 + temp2
+
+value
+
+print (value)
+
+# Відповідь до завдання 20
