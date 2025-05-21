405bd7c53cfeffc6de0f9f5a6a1594b4612bd061
darkistan <darkistan5@gmail.com>
2025-05-21 11:43:35 +0300

    Lesson 23

---
+++ KSM-24-11-23/Lesson 23/Task.py
@@ -0,0 +1,32 @@
+
+# Вправа 1
+
+x = 1
+while x <= 100:
+    print(x)
+    x += 1
+
+
+# Вправа 2
+
+
+    n = int(input("Введіть число більше за 0: "))
+
+if n <= 0:
+    print("Неправильне число")
+else:
+    for i in range(n + 1):
+        print(i)
+
+
+
+# Вправа 3
+
+
+n = int(input("Введіть число більше за 0: "))
+
+if n <= 0:
+    print("Неправильне число")
+else:
+    for i in range(n + 1):
+        print(i)
